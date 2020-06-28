c5f82ac4a38dd99c629aa6d3225466ac67073b87
Nick Chapman <nchapman@gmail.com>
2020-06-28 13:34:38 -0700

    Added category lookup to sources debug.

--- routes/debug/sources.js
+++ routes/debug/sources.js
@@ -1,13 +1,15 @@
 import express from "express";
 import fetch from "isomorphic-unfetch";
+import { getCategoriesMap } from "../../utils.js";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   const response = await fetch("http://localhost:5500/api/sources");
   const sources = await response.json();
+  const categories = await getCategoriesMap();
 
-  res.render("debug/sources/index", { sources });
+  res.render("debug/sources/index", { sources, categories });
 });
 
 export default router;
--- utils.js
+++ utils.js
@@ -1,7 +1,17 @@
-import { existsSync, mkdirSync } from "fs";
+import fs from "fs";
 
 export function initCache() {
-  if (!existsSync(".cache")) {
-    mkdirSync(".cache");
+  if (!fs.existsSync(".cache")) {
+    fs.mkdirSync(".cache");
   }
+}
+
+export async function getCategoriesMap() {
+  const taxonomy = JSON.parse(await fs.promises.readFile("data/taxonomy.json"));
+
+  let map = {};
+
+  taxonomy.forEach(c => map = { ...map, ...c });
+
+  return map;
 }
