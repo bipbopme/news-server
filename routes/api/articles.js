ccfda180f7599c04b86805e8c79407ff0d35ab71
Nick Chapman <nchapman@gmail.com>
2020-07-29 16:58:27 -0700

    Increased minimum reliability in search.

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -115,7 +115,7 @@ router.get("/top", async (req, res) => {
 router.get("/search", async (req, res) => {
   const sources = await sourcesDb.get();
   const sourcesIndex = indexBy(sources, "id");
-  const sourceIds = sources.filter((s) => s.reliability >= 24).map((s) => s.id);
+  const sourceIds = sources.filter((s) => s.reliability >= 32).map((s) => s.id);
 
   const articles = await articlesDb.search(req.query.q, sourceIds);
 
