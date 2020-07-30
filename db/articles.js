1fc0123cc92751d895fa31ebadf26b15e3bfddf0
Nick Chapman <nchapman@gmail.com>
2020-07-29 17:23:23 -0700

    Fixed firstSeenAt.

--- db/articles.js
+++ db/articles.js
@@ -79,7 +79,7 @@ export async function getByIds(ids, size = 1000) {
         "publishDate",
         "url",
         "ampUrl",
-        "firstCrawledAt"
+        "firstSeenAt"
       ],
       size: size
     }
